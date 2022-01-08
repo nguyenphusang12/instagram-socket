5851ea1f4d416d4fb3c6c3f5cbffa9cb90cad065
nguyenphusang12 <nguyenphusang.pnl@gmail.com>
2022-01-08 18:53:44 +0700

    add

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Server } = require("socket.io");
+const PORT = process.env.PORT || 9999;
+const io = new Server(PORT, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+let users = [];
+
+const addUser = (userId, socketId) => {
+  if (!users.some((u) => u.userId === userId)) {
+    users.push({ userId, socketId });
+  } else {
+    const newUser = users.filter((u) => u.userId !== userId);
+    users = newUser;
+    users.push({ userId, socketId });
+  }
+};
+const getUser = (userId) => {
+  const user = users.find((u) => u.userId === userId);
+  return user;
+};
+const removeUser = (socketId) => {
+  users = users.filter((u) => u.socketId !== socketId);
+};
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("addUser", (data) => addUser(data, socket.id));
+  socket.on("sendMessage", ({ recevierId, messages }) => {
+    const user = getUser(recevierId);
+    if (user) {
+      io.to(user.socketId).emit("arrivalMessage", {
+        messages,
+      });
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+    removeUser(socket.id);
+  });
+});
